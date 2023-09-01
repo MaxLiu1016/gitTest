59ab3e9481ee4f848c400d9b7b0d7a4123e15194
Max.l <a0958057936@gmail.com>
2023-09-01 16:09:38 +0800

    init

--- main.py
+++ main.py
@@ -1,16 +1,40 @@
-# 產生1000字元隨機內容
-
 import random
 import os
 
 
-def random_string(num):
-    f = open("test.txt", "w")
-    for i in range(num):
-        f.write(chr(random.randint(0, 127)))
-    f.close()
+def random_string(num, folder="config", filename="config.txt"):
+    # Ensure the folder exists
+    if not os.path.exists(folder):
+        os.makedirs(folder)
+
+    file_path = os.path.join(folder, filename)
+    with open(file_path, "w") as f:
+        for i in range(num):
+            f.write(chr(random.randint(32, 126)))  # Using printable characters
 
 
-# 使用指令產生git分支
-def git_branch(branch_name):
+def git_branch(branch_name, checkout=False):
     os.system("git branch " + branch_name)
+    if checkout:
+        os.system("git checkout " + branch_name)
+
+
+def git_commit(commit_name):
+    os.system("git add .")
+    os.system(f"git commit -m '{commit_name}'")  # Add quotes around commit name
+    os.system("git push")
+
+
+def main():
+    for i in range(10):
+        branch_name = "branch" + str(i)
+        git_branch(branch_name, True)
+        for j in range(10):
+            random_string(1000)
+            commit_msg = f"{branch_name} commit {j}"
+            git_commit(commit_msg)
+    os.system("git checkout master")
+
+
+    main()
+
